1f805972d93f646e91d7e0a03eb105aca7eafde4
shoalieh <124621042+shoalieh@users.noreply.github.com>
2023-11-28 12:06:02 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,20 @@
 // service-worker.js
-
-const CACHE_NAME = "offline-cache";
-const OFFLINE_URL = "/offline.html";
-const NOTIFICATION_WORKER_PATH = "/notification-worker.js";
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([OFFLINE_URL]);
+        caches.open('offline-cache').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html'
+                // افزودن فایل‌های دیگر به دلخواه
+            ]);
         })
     );
-    self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-    if (event.request.mode === "navigate") {
-        event.respondWith(
-            fetch(event.request)
-                .then((response) => {
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-                })
-                .catch(() => {
-                    return caches.match(new Request(OFFLINE_URL));
-                })
-        );
-    }
-});
-
-self.addEventListener("offline", (event) => {
-    clients.matchAll({ type: "window" }).then((clients) => {
-        clients.forEach((client) => {
-            if (client.url === self.location.href) {
-                client.navigate(OFFLINE_URL);
-            }
-        });
-    });
-
-    clients.matchAll().then((clients) => {
-        clients.forEach((client) => {
-            client.postMessage({ type: "SHOW_OFFLINE_NOTIFICATION" });
-        });
-    });
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        navigator.serviceWorker.register(NOTIFICATION_WORKER_PATH)
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
     );
-});
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
 });
